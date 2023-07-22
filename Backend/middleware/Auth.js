f2527ffa959115ec7bda0b197e0ac97c3dcd340f
shakirva <vashakir245@gmail.com>
2023-07-22 16:01:05 +0530

    justlast changed

--- Backend/middleware/Auth.js
+++ Backend/middleware/Auth.js
@@ -1,23 +1,31 @@
-const ErrorHander=require('../utils/errorhandler');
-const catchAsyncErrors=require('./catchAsyncErrors');
-const User=require('../models/usermodels');
-const jwt=require('jsonwebtoken');
+const ErrorHandler = require("../utils/errorhandler");
+const catchAsyncErrors = require("./catchAsyncErrors");
+const JWT = require("jsonwebtoken");
+const User = require("../models/userModel");
 
 
-exports.isAuthenticatedUser=catchAsyncErrors(async(req,res,next)=>{
-    const {token}=req.cookies;
-    if(!token){
-        return next(new ErrorHander('Login first to access this resource',401));
+exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
+
+    const { token } = req.cookies;
+
+    if (!token) {
+        return next(new ErrorHandler("Please Login to access this resource", 401));
     }
-    const decoded=jwt.verify(token,process.env.JWT_SECRET);
-    req.user=await User.findById(decoded.id);
+
+    const decodeData = JWT.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decodeData.id);
+
     next();
-});     
-exports.authorizeRoles=(...roles)=>{
-    return(req,res,next)=>{
-        if(!roles.includes(req.user.role)){
-            return next(new ErrorHander(`Role (${req.user.role}) is not allowed to access this resource`,403));
+})
+
+
+exports.authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if (!roles.includes(req.user.role)) {
+            return next(new ErrorHandler(`Role : ${req.user.role} is not allowed to access this resource`, 403));
         }
+
         next();
     }
 }
