d04a2501171012a46d248dbe8a250188db28b84e
shakirva <vashakir245@gmail.com>
2023-07-22 15:25:45 +0530

    justlast changedd

--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -1,43 +1,37 @@
-const ErrorHander = require('../utils/errorHander');
+const ErrorHandler = require("./utils/errorhandler");
 
+module.exports = (err, req, res, next) => {
+    //This called a middleware  
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error"
 
-module.exports=(err,req,res,next)=>{
-    err.statusCode=err.statusCode || 500;
-    err.message=err.message || 'Internal Server Error'
-
-    // mongodb id error
-    if(err.name==='CastError'){
-        const message=`Resource not found. Invalid: ${err.path}`;
-        err= new ErrorHander(message,400); 
-        
-    }
-    // mongoose validation error
-    if(err.name==='ValidationError'){           
-        const message=Object.values(err.errors).map(value=>value.message);
-        err= new ErrorHander(message,400);
-    }
-    // mongoose duplicate key error
-    if(err.code===11000){
-        const message=`Duplicate ${Object.keys(err.keyValue)} entered`;
-        err= new ErrorHander(message,400);
-    }
-    // wrong jwt error
-    if(err.name==='JsonWebTokenError'){
-        const message='JSON Web Token is invalid. Try Again!!!';
-        err= new ErrorHander(message,400);
-    }
-    // jwt expired error
-    if(err.name==='TokenExpiredError'){
-        const message='JSON Web Token is expired. Try Again!!!';
-        err= new ErrorHander(message,400);
+    //Wrong MOngodb Erro (Important)
+    if (err.name === "CastError") {
+        const message = `Resource Not Forund ${err.path}`;
+        err = new ErrorHandler(message, 400);
     }
 
-    
-    
+    //Mongoose duplicate key error
+    if (err.code === 11000) {
+        const message = `Duplicate ${Object.keys(err.keyValue)} Entered`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    //Wrong JWT Token Error
+    if (err.name === "JsonWebTokenError") {
+        const message = `Json Web Token Is Invalid , try again.`;
+        err = new ErrorHandler(message, 400);
+    }
+
+    //JWT Expire Error
+    if (err.name === "TokenExpiredError") {
+        const message = `Json Web Token Is Expired , try again.`;
+        err = new ErrorHandler(message, 400);
+    }
 
 
     res.status(err.statusCode).json({
-        success:false,
-        Error:err.message  
+        success: true,
+        message: err.message,// we can also use .slack (try this and see what happening....)
     });
 };
