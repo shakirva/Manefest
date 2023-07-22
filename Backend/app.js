81df874d7036492c8fc4ab970a7d3678037f1d95
shakirva <vashakir245@gmail.com>
2023-07-22 07:38:59 +0530

    justlast changedd

--- Backend/app.js
+++ Backend/app.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 
 
 
-app.use(express.json());
+
 app.use(cookieParser());
 
 app.use(bodyParser.json({ limit: "50mb" }))
@@ -39,19 +39,20 @@ app.use("/api/v1", payment);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-app.get("*", (req,res) => {
-  res.sendFile(path.resolve(__dirname, "../frontend/build/index.tml"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
 });
 
-// const corsOptions = {
-//   origin: '*',
-//   credentials: true,
-//   optionSuccessStatus: 200
-// }
-// app.use(cors(corsOptions))
-// app.use(bodyParser.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.set('trust proxy', 1);
+
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200
+}
+app.use(cors(corsOptions))
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('trust proxy', 1);
 
 
 //middleware for erros
