abea7829ba6b5aa0ccb15ba71d46bc5b3582a117
shakirva <vashakir245@gmail.com>
2023-08-03 00:31:21 +0530

    Fix file name casing issues

--- backend/route/orderroutes.js
+++ backend/route/orderroutes.js
@@ -1,21 +1,21 @@
-    const express = require("express");
-    const { newOrder, getSingleOrder, myOrders, getAllOrders, updateOrder, deleteOrder } = require("../controller/ordercontroller");
-    const router = express.Router();
+const express = require("express");
+const { newOrder, getSingleOrder, myOrders, getAllOrders, updateOrder, deleteOrder } = require("../controller/ordercontroller");
+const router = express.Router();
 
-    const { isAuthenticatedUser, authorizeRoles } = require('../Middleware/auth.js');
+const { isAuthenticatedUser, authorizeRoles } = require('../Middleware/auth.js');
 
 
-    router.route("/order/new").post(isAuthenticatedUser, newOrder);
+router.route("/order/new").post(isAuthenticatedUser, newOrder);
 
-    router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
+router.route("/order/:id").get(isAuthenticatedUser, getSingleOrder);
 
-    router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
 
-    router.route("/admin/orders").get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
+router.route("/admin/orders").get(isAuthenticatedUser, authorizeRoles("admin"), getAllOrders);
 
-    router.route("/admin/order/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder);
+router.route("/admin/order/:id").put(isAuthenticatedUser, authorizeRoles("admin"), updateOrder);
 
-    router.route("/admin/order/:id").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
+router.route("/admin/order/:id").delete(isAuthenticatedUser, authorizeRoles("admin"), deleteOrder);
 
 
 
